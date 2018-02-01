4e09b1534e1c2617917ea7c3f5f7425a7f006ddd
chengaohua <qq1213908760@163.com>
2018-02-01 20:42:17 +0800

    添加注释

--- utils/uls_decrypt.h
+++ utils/uls_decrypt.h
@@ -5,12 +5,39 @@
 #ifndef AES256_ULS_DECRYPT_H
 #define AES256_ULS_DECRYPT_H
 
+/**
+ * 解密
+ * @param input　　　　　[in]   输入文件
+ * @param output　　　　 [in]   输出文件
+ * @param key　　　　　　[in]    密钥
+ * @param keyLength    [in]　　 密钥长度
+ * @return      <0:error
+ */
 int uls_decrypt_file2file(const char * input, const char * output,
                           unsigned char * key, int keyLength);
 
+/**
+ * 解密
+ * @param input　　　　　　　[in]   　　输入文件名
+ * @param outputBuf        [in]　　　　输出缓冲区地址
+ * @param outputLen        [in/out]　　入参作为缓冲区大小，出参作为解密后数据大小
+ * @param key              [in]　　　　密钥
+ * @param keyLen           [in]　　　　密钥长度
+ * @return 　　<0 : error
+ */
 int uls_decrypt_file2mem(const char * input, char * outputBuf, int * outputLen,
                          unsigned char * key, int keyLen);
 
+/**
+ * 解密
+ * @param inputBuf　　　　　　　[in]  　　　　 输入缓冲区地址
+ * @param inputLen            [in]　　　　　输入缓冲区大小
+ * @param outputBuf           [out]　　　　输出缓冲区地址
+ * @param outputLen           [in/out]　　入参作为缓冲区大小，出参作为解密后数据大小
+ * @param key                 [in]　　　　　密钥　
+ * @param keyLen              [in]　　　　　密钥长度　
+ * @return
+ */
 int uls_decrypt_mem2mem(char * inputBuf, int inputLen, char * outputBuf, int * outputLen,
                         unsigned char * key, int keyLen);
 
--- utils/uls_encrypt.h
+++ utils/uls_encrypt.h
@@ -7,8 +7,17 @@
 
 #include <stdint.h>
 
+
+/**
+ * 加密文件（若是比较大的二进制文件，建议部分加密）
+ * @param input 　　      [in]  输入文件，未加密文件
+ * @param output 　　　　  [in]   输出文件，加密后文件
+ * @param key 　　　　　　 [in]   密钥
+ * @param keyLength 　　  [in]   密钥长度
+ * @param blockSize      [in]    要加密数据块大小（<=0 则全部加密）
+ * @return 　　<0 :error;
+ */
 int uls_encrypt_file2file(const char * input, const char * output,
                           unsigned char * key, int keyLength, int64_t blockSize);
-
 
 #endif //AES256_ULS_ENCRYPT_H
